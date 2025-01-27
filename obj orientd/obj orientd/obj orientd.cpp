9cca5fb0e01303014f3154937c5eadb014d2bf4b
Sultan <110305964+AbdulWahabsultan@users.noreply.github.com>
2025-01-27 23:00:47 +0500

    OOP
    Learnt about copying constructors and delegating constructors

--- obj orientd/obj orientd/obj orientd.cpp
+++ obj orientd/obj orientd/obj orientd.cpp
@@ -8,13 +8,24 @@ private:
 	int health;
 	int xp;
 public:
-	player();
-	player(std::string name_val);
-	player(std::string name_val, int health_val, int xp_val);
-
+	//player();
+	//player(std::string name_val);
+	//player(std::string name_val, int health_val, int xp_val);
+	//Made constructor parameters and default value
+	//	player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+	
+	//copying constructor
+	std::string get_name() { return name; }
+	int get_health() { return health; }
+	int get_xp() { return xp; }
+	player(std::string name_val = "None", int heatlh_val = 0, int xp_val = 0);
+	//copy constroctor
+	player(const player& source);
+	//Destroctor
+	~player() { std::cout << "Destroctor called for " << name << std::endl; }
 };
 
-player::player()
+/*player::player()
 {
 	name = "Empty";
 	health = 0;
@@ -40,9 +51,20 @@ player::player(std::string name_val)
 
 }*/
 player::player(std::string name_val, int xp_val, int health_val)
-	:name{ name_val }, health{ health_val }, xp{ xp_val } {
+	:name{ name_val}, health{ health_val }, xp{ xp_val } {
 	std::cout << "\n" << name << health << xp;
  }
+player::player(const player &soruce) 
+	:name{soruce.name},health{soruce.health},xp{soruce.xp}
+{
+	std::cout << "Copy constructor made for :" << soruce.name;
+}
+void display_player(player p)
+{
+	std::cout << "Name" << p.get_name() << std::endl;
+	std::cout << "Health" << p.get_health() << std::endl;
+	std::cout << "Xp" << p.get_xp() << std::endl;
+}
 int main()
 {
 	player empty;
